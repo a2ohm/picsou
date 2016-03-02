3869f198c82ccb40d80cdb5aff2ee24b83049a37
a2ohm <antoine@2ohm.fr>
2016-03-02 11:45:18 +0100

    Setup a basic command line parser for picsou (parser for the 'add' command)

--- picsou.py
+++ picsou.py
@@ -4,5 +4,22 @@
 import argparse
 
 # Parse arguments
-parser = argparse.ArgumentParser()
-parser.parse_args()
+# Create the top-level parser
+parser = argparse.ArgumentParser(prog="picsou")
+subparsers = parser.add_subparsers()
+
+# Create the parser for the "add" command
+parser_add = subparsers.add_parser("add",
+        help="add a gain or a spending")
+group_add_SG = parser_add.add_mutually_exclusive_group()
+group_add_SG.add_argument('-s', '--spend', type=float,
+        help="amount of the spending")
+group_add_SG.add_argument('-g', '--gain', type=float,
+        help="amount of the gain")
+
+args = parser.parse_args()
+
+if args.spend:
+    print("spend: -%.2f €" % args.spend)
+elif args.gain:
+    print("gain:  +%.2f €" % args.gain)
