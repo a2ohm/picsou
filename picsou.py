5cb2d3675231ed54544a67d3adbd79c43298e281
a2ohm <antoine@2ohm.fr>
2016-03-02 12:04:42 +0100

    A function has to be dedicated to each subcommand

--- picsou.py
+++ picsou.py
@@ -2,6 +2,16 @@
 # -*- coding:utf-8 -*-
 
 import argparse
+import readline
+
+# Sub-command functions
+def add(args):
+    """Add a gain or a spending.
+    """
+    if args.spend:
+        print("spend: -%.2f €" % args.spend)
+    elif args.gain:
+        print("gain:  +%.2f €" % args.gain)
 
 # Parse arguments
 # Create the top-level parser
@@ -11,6 +21,8 @@ subparsers = parser.add_subparsers()
 # Create the parser for the "add" command
 parser_add = subparsers.add_parser("add",
         help="add a gain or a spending")
+parser_add.set_defaults(func=add)
+
 group_add_SG = parser_add.add_mutually_exclusive_group()
 group_add_SG.add_argument('-s', '--spend', type=float,
         help="amount of the spending")
@@ -18,8 +30,4 @@ group_add_SG.add_argument('-g', '--gain', type=float,
         help="amount of the gain")
 
 args = parser.parse_args()
-
-if args.spend:
-    print("spend: -%.2f €" % args.spend)
-elif args.gain:
-    print("gain:  +%.2f €" % args.gain)
+args.func(args)
