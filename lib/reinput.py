9a41535d751b272325f808893c902e4ba1d307a6
a2ohm <antoine@2ohm.fr>
2016-03-03 11:23:19 +0100

    A custom input function can be used in reinput.

--- lib/reinput.py
+++ lib/reinput.py
@@ -6,7 +6,7 @@ My version of input.
 """
 
 def reinput(prompt, prompt_end = ": ",
-        valid = None, default = None, type=str):
+        valid = None, default = None, type=str, func=input):
     """Print the prompt and wait for inputs from the user until this
     input is valid. If the input is empty, default is returned (if
     given).
@@ -29,15 +29,33 @@ def reinput(prompt, prompt_end = ": ",
     # Get the input
     if valid:
         while cin not in valid:
-            cin = input(prompt)
+            cin = func(prompt)
     elif default:
-        cin = input(prompt)
+        cin = func(prompt)
     else:
         while cin in ['', None]:
-            cin = input(prompt)
+            cin = func(prompt)
 
     # Return the default value if required
     if default and cin == '':
         return default
     else:
         return type(cin)
+
+if __name__ == '__main__':
+    # Basic usage
+    cin = reinput("Six fois sept font...", default=42, type=int)
+    print("Vous avez dit %d ?" % cin)
+
+    # Now, with cinput
+    from cinput import cinput
+    valid_numbers = ['quatre', 'vingt', 'quarante-deux', 'un']
+
+    def my_cinput(prompt):
+        valid_numbers = ['quatre', 'vingt', 'quarante-deux', 'un']
+        return cinput(prompt, valid_numbers)
+
+    cin = reinput("Quel est le nombre après deux ?",
+            default="huit", valid=valid_numbers, func=my_cinput)
+    print("Vous avez dit %s ?" % cin)
+
