e1b758d5ba75c12a280500def78f1c0d2c9743a1
a2ohm <antoine@2ohm.fr>
2016-03-07 13:52:57 +0100

    Create the stager class

---
+++ lib/stager.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python3
+# -*- coding:utf-8 -*-
+
+class stager():
+    """Handle the stage file.
+
+    New spending are added in a stage file. They have to be commited to
+    be pulled in the database.
+    """
+
+    def __init__():
+        pass
+
+if __name__ == '__main__':
+    pass
