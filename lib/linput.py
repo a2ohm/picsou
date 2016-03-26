4ded82020e8ae19ae1b01c8c89deb248549b8480
a2ohm <antoine@2ohm.fr>
2016-03-26 23:15:46 +0100

    Implement linput to get a list of data item by item

---
+++ lib/linput.py
@@ -0,0 +1,48 @@
+#! /usr/bin/python3
+# -*- coding:utf-8 -*-
+
+def linput(prompt, prompt_end = ": ",
+        valid = None, type=str, func=input, **kw):
+    """Print the prompt and wait for valid inputs from the user until
+    this input is empty. Then a list a all inputs is returned.
+    """
+
+    cin = None
+    inputs = []
+
+    prompt += prompt_end
+
+    if valid:
+        valid += ['']
+
+        while cin != '':
+            cin = None
+            while cin not in valid:
+                cin = func(prompt, **kw)
+
+            if cin == '':
+                break
+            else:
+                inputs += [type(cin)]
+    else:
+        while cin != '':
+            cin = type(func(prompt, **kw))
+
+            if cin != '':
+                inputs += [cin]
+            else:
+                break
+
+    return inputs
+
+if __name__ == '__main__':
+    cin = linput("Tags", valid = ['a', 'b', 'c'])
+    print(cin)
+
+    # Now with cinput
+    from cinput import cinput
+    valid_letters = ['a', 'b', 'c']
+
+    cin = linput("Tags",
+            valid=valid_letters, func=cinput, complete=valid_letters)
+    print(cin)
